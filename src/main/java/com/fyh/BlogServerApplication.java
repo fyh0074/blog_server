ec93f86f01a18f368091cd455b3843c8d469c788
fyh0074 <fyh0074@gmail.com>
2023-10-12 00:27:28 +0900

    7

--- src/main/java/com/fyh/BlogServerApplication.java
+++ src/main/java/com/fyh/BlogServerApplication.java
@@ -5,25 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-// @SpringBootApplication
-// public class BlogServerApplication extends SpringBootServletInitializer {
-
-//     @Override
-//     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//         return application.sources(BlogServerApplication.class);
-//     }
-
-//     public static void main(String[] args) throws Exception {
-//         SpringApplication.run(BlogServerApplication.class, args);
-//     }
-
-// }
-
 @SpringBootApplication
-public class BlogServerApplication {
+public class BlogServerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogServerApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BlogServerApplication.class, args);
 	}
 
 }
+
+//@SpringBootApplication
+//public class BlogServerApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BlogServerApplication.class, args);
+//	}
+//
+//}
