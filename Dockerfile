34d80c8d8ae4b12fc33036d7a462c7c8c6302b60
fyh0074 <fyh0074@gmail.com>
2023-10-11 23:03:19 +0900

    2023/10/11 初期化

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM maven:3.8.5-openjdk-17-slim AS builder
+
+# 构建的工作目录设置为/app
+WORKDIR /app
+
+# 首先只复制 pom.xml 并下载依赖
+COPY pom.xml .
+RUN mvn dependency:go-offline
+
+# 然后复制整个项目并构建应用
+COPY src ./src
+RUN mvn package
+
+# 在这里添加查看文件列表的命令
+RUN ls -al /app/target
+
+# 使用 Jetty 镜像
+FROM jetty:11.0-jre17-alpine
+
+# 复制构建后的 WAR 文件到 Jetty 的 webapps 目录中
+COPY --from=builder /app/target/blog_server.war /var/lib/jetty/webapps/root.war
+
+# 暴露 Jetty 的默认端口
+EXPOSE 8080
+
+# 定义启动命令，不需要CMD ["ls","-al","|","echo"]
---
+++ src/main/java/com/fyh/BlogServerApplication.java
@@ -0,0 +1,29 @@
+package com.fyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BlogServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BlogServerApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BlogServerApplication.class, args);
+    }
+
+}
+
+//@SpringBootApplication
+//public class BlogServerApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BlogServerApplication.class, args);
+//	}
+//
+//}
